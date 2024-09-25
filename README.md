0356589236ea723f41e1f0b73ccda4b15ee1b333
PaulKalho <kalhornpaul@gmail.com>
2024-09-25 21:08:12 +0200

    feat: init repo

---
+++ README.md
@@ -0,0 +1,14 @@
+### Host
+
+This is the repository where I define all my important hosts on my server.
+
+### How-To
+
+Install nix on your system.
+
+To jump into the devshell: `nix develop`
+
+1) To encrypt and decrypt files you have to create a PGP Key.
+Edit the .sops.yaml accordingly.
+
+2) We use [opentofu](https://opentofu.org/) as our IaC tool.
---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+        sops-nix.url = "github:Mic92/sops-nix";
+    };
+    outputs = { self, nixpkgs, sops-nix }:
+    let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in {
+        devShells.${system}.default = pkgs.mkShell {
+            buildInputs = [
+                pkgs.sops
+                pkgs.opentofu
+            ];
+        };
+    };
+}
